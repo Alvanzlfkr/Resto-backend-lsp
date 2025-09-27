481234e2a4c2d7011fb164d059116738bdbdde9a
Alvanzlfkr <mohamadalvanzulfikhar@gmail.com>
2025-09-28 01:21:17 +0700

    perubahan route home - dashboard

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -1,10 +1,10 @@
 <div class="main-sidebar sidebar-style-2">
     <aside id="sidebar-wrapper">
         <div class="sidebar-brand">
-            <a href="index.html">Dapur Bunda Bahagia</a>
+            <a href="{{ url('/') }}">Dapur Bunda Bahagia</a>
         </div>
         <div class="sidebar-brand sidebar-brand-sm">
-            <a href="index.html">Alvan</a>
+            <a href="{{ url('/') }}">Alvan</a>
         </div>
         <ul class="sidebar-menu">
             <li class="menu-header">Dashboard</li>
@@ -12,9 +12,9 @@
                 <a href="#"
                     class="nav-link has-dropdown"><i class="fas fa-fire"></i><span>Dashboard</span></a>
                 <ul class="dropdown-menu">
-                    <li class='{{ Request::is('dashboard-general-dashboard') ? 'active' : '' }}'>
+                    <li class='{{ Request::is('home') ? 'active' : '' }}'>
                         <a class="nav-link"
-                            href="{{ url('dashboard-general-dashboard') }}">Dashboard</a>
+                            href="{{ url('home') }}">Dashboard</a>
                     </li>
                 
                 </ul>
